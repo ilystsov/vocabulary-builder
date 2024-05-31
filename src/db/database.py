4f5b867eea0af0a86542a8f5b023459710d433e1
ilya <lystsov.ilya@gmail.com>
2024-05-31 23:21:10 +0300

    [FEATURE] Add sqlalchemy orm infrastructure

---
+++ src/db/database.py
@@ -0,0 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+BaseModel = declarative_base()
