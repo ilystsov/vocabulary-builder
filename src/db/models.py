105abd9c18c3910a4ac42d057e2d4a5d3df4b159
ilya <lystsov.ilya@gmail.com>
2024-06-01 11:32:21 +0300

    [FEATURE] Update WordModel with UUID default

--- src/db/models.py
+++ src/db/models.py
@@ -1,3 +1,4 @@
+import uuid
 from uuid import UUID
 
 from sqlalchemy.orm import Mapped, mapped_column
@@ -7,7 +8,7 @@ from src.db.database import BaseModel
 
 class WordModel(BaseModel):
     __tablename__ = 'words'
-    id: Mapped[UUID] = mapped_column(primary_key=True)
+    id: Mapped[UUID] = mapped_column(default=uuid.uuid4, primary_key=True)
     word: Mapped[str] = mapped_column(nullable=False)
     translated_word: Mapped[str] = mapped_column(nullable=False)
     context: Mapped[str] = mapped_column(nullable=False)
