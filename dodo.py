ae5943f9da5cce1159ef94c577e017da4733bb0b
ilya <lystsov.ilya@gmail.com>
2024-06-10 19:16:55 +0300

    [FEATURE] Add task automatization via doit

---
+++ dodo.py
@@ -0,0 +1,45 @@
+import glob
+import shutil
+
+from doit.task import clean_targets
+from doit.tools import create_folder
+LOCALES_DIRECTORY = 'src/locales'
+
+
+def task_pot():
+    """Recreate .pot file."""
+    return {
+        'actions': ['pybabel extract --mapping babel.cfg -o translations.pot src'],
+        'file_dep': glob.glob('src/templates/*.html'),
+        'targets': ['translations.pot'],
+        'clean': [clean_targets],
+    }
+
+
+def task_po():
+    """Update translations."""
+    return {
+        'actions': ['pybabel update -D translations -d locales -l fr -i translations.pot '],
+        'file_dep': ['translations.pot'],
+        'targets': ['locales/fr/LC_MESSAGES/translations.po'],
+    }
+
+
+def task_mo():
+    """Compile translations."""
+    return {
+        'actions': [
+            (create_folder, [f'{LOCALES_DIRECTORY}/fr/LC_MESSAGES']),
+            f'pybabel compile -D translations -l fr -i locales/fr/LC_MESSAGES/translations.po -d {LOCALES_DIRECTORY}'
+        ],
+        'file_dep': ['locales/fr/LC_MESSAGES/translations.po'],
+        'targets': [f'{LOCALES_DIRECTORY}/fr/LC_MESSAGES/translations.mo'],
+        'clean': [clean_targets],
+    }
+
+
+def task_i18n():
+    return {
+        'actions': [],
+        'task_dep': ['mo', 'po', 'pot'],
+    }
