935acfaa287f7d3b96fa4409deb04e752fe0fc44
ksuglobov <ksuglobov@outlook.com>
2024-06-22 15:07:04 +0300

    Fix file styles as per pre-commit hooks

--- README.md
+++ README.md
@@ -1,27 +1,29 @@
 ## Vocabulary Builder
+
 ### Project Overview
+
 The goal of this project is to develop a website dedicated to facilitating English language learning.
 
 #### Key Features:
 
-- Retrieving a random word with an example of its use.
-- User registration and saving favorite words.
-- Testing the user on learned words.
+-   Retrieving a random word with an example of its use.
+-   User registration and saving favorite words.
+-   Testing the user on learned words.
 
 ### Anticipated Stack
 
-- FastAPI
-- PostgreSQL
-- SQLAlchemy
-- alembic
-- Jinja2
-- HTML
-- CSS
-- JavaScript
-- Docker
+-   FastAPI
+-   PostgreSQL
+-   SQLAlchemy
+-   alembic
+-   Jinja2
+-   HTML
+-   CSS
+-   JavaScript
+-   Docker
 
 ### Interface Mockup
-![][interface_draft]
 
+![][interface_draft]
 
 [interface_draft]: assets/interface_draft.png
--- src/static/js/new_word.js
+++ src/static/js/new_word.js
@@ -1,8 +1,11 @@
-document.getElementById('new-word-btn').addEventListener('click', async function() {
-    const language = window.location.pathname.split('/')[1];
-    const response = await fetch(`/${language}/new_word`);
-    const data = await response.json();
-    document.getElementById('word').textContent = data.word;
-    document.getElementById('context').textContent = data.context;
-    document.getElementById('translated_context').textContent = data.translated_context;
-});
+document
+    .getElementById('new-word-btn')
+    .addEventListener('click', async function () {
+        const language = window.location.pathname.split('/')[1];
+        const response = await fetch(`/${language}/new_word`);
+        const data = await response.json();
+        document.getElementById('word').textContent = data.word;
+        document.getElementById('context').textContent = data.context;
+        document.getElementById('translated_context').textContent =
+            data.translated_context;
+    });
