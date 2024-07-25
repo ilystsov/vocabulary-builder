02d8fb3e96a792c568604d6ef0b37d40cdd2ec91
ilya <lystsov.ilya@gmail.com>
2024-07-25 14:35:46 +0500

    [REFACTOR] Delete redundant /error endpoint

--- vocabulary_builder/main.py
+++ vocabulary_builder/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from starlette.requests import Request
-from starlette.responses import HTMLResponse, JSONResponse, RedirectResponse
+from starlette.responses import JSONResponse, RedirectResponse
 
 from vocabulary_builder.routes import auth, pages, users, words
 
@@ -35,17 +35,5 @@ async def custom_http_exception_handler(request: Request, exc: StarletteHTTPExce
     :return: JSON response or redirect to error page.
     """
     if exc.status_code in {404, 422}:
-        return RedirectResponse(url="/error")
+        return RedirectResponse(url="/page_not_found")
     return JSONResponse(status_code=exc.status_code, content={"detail": exc.detail})
-
-
-@app.get("/error", response_class=HTMLResponse)
-def error_page(request: Request):
-    """
-    Serve the error page.
-
-    :param request: HTTP request.
-    :return: HTML response with the error page.
-    """
-    response = RedirectResponse(url="/page_not_found", status_code=303)
-    return response
