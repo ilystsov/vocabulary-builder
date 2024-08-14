4cceaf682d4a377e9c156f40432a6181b7c77800
ilya <lystsov.ilya@gmail.com>
2024-08-14 20:06:24 +0500

    [CHORE] Remove SQLite-specific connection argument from SQLAlchemy engine setup

--- vocabulary_builder/db/database.py
+++ vocabulary_builder/db/database.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 BaseModel = declarative_base()
